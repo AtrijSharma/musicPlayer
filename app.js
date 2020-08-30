6443e7fc5f76d710422a26be28d57c8a49c2f583
Atrij Sharma <1999atrijsharma@gmail.com>
2020-08-30 10:27:59 +0530

    music

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const express=require('express')
 const app=express()
+const httpServer = require('http').createServer(app);
 
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 
-let todo=[]
+
 
 app.use('/',express.static(__dirname+'/static'))
 
 
 let port =  process.env.PORT || 4444 
-app.listen(port,()=>{
+httpServer.listen(port,()=>{
     console.log("http://localhost:4444")
 })
